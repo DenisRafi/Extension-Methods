b777af234714d6bb34319d818a3686178d0aedce
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-06-01 15:48:22 +0100

    Add files via upload

---
+++ Extension Methods.cs
@@ -0,0 +1,28 @@
+﻿/*
+C# Extension Methods
+
+Example shows how to call the standard query operator.
+OrderBy method on an array of integers.
+The expression in parentheses is a lambda expression.
+Many standard query operators take lambda expressions as parameters, 
+but this isn't a requirement for extension methods.
+ 
+By Denis Rafi
+ */
+using System.Linq;
+namespace CA
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = ints.OrderBy(g => g);
+            foreach (var i in result)
+            {
+                System.Console.Write(i + " ");
+            }
+        }
+    }
+}
+//Output: 1 2 3 4 5 6 7 8 9 10
